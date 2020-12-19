a670481f99ef63c0bf07b99b158a7c7cc913c6fd
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2020-12-19 14:33:18 +0300

    Added tests.

---
+++ Test/SentiNetTest.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Olcay Taner YILDIZ on 19.12.2020.
+//
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+#include "../SentiNet.h"
+
+TEST_CASE("SentiNet-getPositives") {
+    SentiNet sentiNet = SentiNet();
+    REQUIRE(3100 == sentiNet.getPositives().size());
+}
+
+TEST_CASE("SentiNet-getNegatives") {
+    SentiNet sentiNet = SentiNet();
+    REQUIRE(10191 == sentiNet.getNegatives().size());
+}
+
+TEST_CASE("SentiNet-getNeutrals") {
+    SentiNet sentiNet = SentiNet();
+    REQUIRE(63534 == sentiNet.getNeutrals().size());
+}
---
+++ Test/SentiSynSetTest.cpp
@@ -0,0 +1,30 @@
+//
+// Created by Olcay Taner YILDIZ on 19.12.2020.
+//
+
+#include <fstream>
+#include "catch.hpp"
+#include "../SentiNet.h"
+
+TEST_CASE("SentiSynSet-saveAsXml") {
+    string line;
+    ifstream infile;
+    ofstream outFile;
+    SentiNet sentiNet = SentiNet();
+    outFile.open("test.xml", ofstream::out);
+    SentiSynSet sentiSynSet = sentiNet.getSentiSynSet("TUR10-1093230");
+    sentiSynSet.saveAsXml(outFile);
+    sentiSynSet = sentiNet.getSentiSynSet("TUR10-0730690");
+    sentiSynSet.saveAsXml(outFile);
+    sentiSynSet = sentiNet.getSentiSynSet("TUR10-0969360");
+    sentiSynSet.saveAsXml(outFile);
+    outFile.close();
+    infile.open("test.xml", ifstream::in);
+    getline(infile, line);
+    REQUIRE("<SYNSET><ID>TUR10-1093230</ID><PSCORE>0.25</PSCORE><NSCORE>0</NSCORE></SYNSET>" == line);
+    getline(infile, line);
+    REQUIRE("<SYNSET><ID>TUR10-0730690</ID><PSCORE>0</PSCORE><NSCORE>0</NSCORE></SYNSET>" == line);
+    getline(infile, line);
+    REQUIRE("<SYNSET><ID>TUR10-0969360</ID><PSCORE>0</PSCORE><NSCORE>1</NSCORE></SYNSET>" == line);
+    infile.close();
+}
